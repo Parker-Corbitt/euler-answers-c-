107d1e5f2f1327dc367de98f7f418e3a293d3c6c
Parker-Corbitt <parkercorbitt36@gmail.com>
2023-03-15 12:51:04 -0400

    problem 3 added

---
+++ euler3/euler_3.cc
@@ -0,0 +1,66 @@
+/**
+ *        @file: euler3.cc
+ *      @author: Parker Corbitt
+ *        @date: March 13, 2023
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <math.h>
+#include <omp.h>
+using namespace std;
+
+/// function prototypes
+
+long long largestFactor = 0;
+
+void check_factor(long long number, long long j);
+bool is_prime(long long j);
+
+int main(int argc, char const *argv[])
+{
+    
+    long long number = 0;
+    cout << "Enter the number to find its largest prime factor" << endl;
+    cin >> number;
+
+    long long j = 2;
+    while(j <= (number / 2))
+    {
+        check_factor(number, j);
+        j++;
+    }
+    cout << largestFactor << endl;
+    return 0;
+}
+
+void check_factor(long long number, long long j)
+{
+    if(number % j == 0)
+    {
+        if((j > largestFactor) && (is_prime(j)))
+        {
+            largestFactor = j;
+        }
+    }
+}
+
+bool is_prime(long long j)
+{
+    if(j >= 2)
+    {
+        #pragma omp parallel for schedule(dynamic) num_threads(16)
+        for(int i = 2; i <= (j / 2); i++)
+        {
+            if(j % i == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//600851475143
