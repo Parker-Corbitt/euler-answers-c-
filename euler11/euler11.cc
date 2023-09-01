c57ec3deaa5afecacb91905e1ad422cd7ac70c5c
Parker-Corbitt <parkercorbitt36@gmail.com>
2023-09-01 09:39:26 -0400

    Correct answer found, cleaning up

--- euler11/euler11.cc
+++ euler11/euler11.cc
@@ -18,6 +18,10 @@ int NUM_COLUMN = 19;
 
 void load_vector(ifstream &input, vector<vector<int>> &grid);
 void print_vector(vector<vector<int>> &grid);
+int check_down(vector<vector<int>> &grid);
+int check_ltor(vector<vector<int>> &grid);
+int check_bl_to_tr(vector<vector<int>> &grid);
+int check_br_to_tl(vector<vector<int>> &grid);
 
 int main(int argc, char const *argv[])
 {
@@ -34,12 +38,17 @@ int main(int argc, char const *argv[])
 
     load_vector(input, grid);
     print_vector(grid);
+    cout << '\n' << check_down(grid) << endl;
+    cout << '\n' << check_ltor(grid) << endl;
+    cout << '\n' << check_bl_to_tr(grid) << endl;
+    cout << '\n' << check_br_to_tl(grid) << endl;
 
     return 0;
 } // main
 
 //************************************************************************************************
 
+//The 2D vector should be structured as if i represents the rows and j the column
 void load_vector(ifstream &input, vector<vector<int>> &grid)
 {
     string tmp;
@@ -76,16 +85,68 @@ void print_vector(vector<vector<int>> &grid)
 
 int check_down(vector<vector<int>> &grid)
 {
+    int max = 0;
+    for(int i = 0; i < grid.size() - 4; i++)
+    {
+        for(int j = 0; j < grid[i].size(); j++)
+        {
+            int x = grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j];
+            if(x > max)
+            {
+                max = x;
+            }
+        }
+    }
+    return max;
 }
 
 int check_ltor(vector<vector<int>> &grid)
 {
+    int max = 0;
+    for(int i = 0; i < grid.size(); i++)
+    {
+        for(int j = 0; j < grid[i].size() - 4; j++)
+        {
+            int x = grid[i][j] * grid[i][j + 1] * grid[i][j + 2] * grid[i][j + 3];
+            if(x > max)
+            {
+                max = x;
+            }
+        }
+    }
+    return max;
 }
 
 int check_bl_to_tr(vector<vector<int>> &grid)
 {
+    int max  = 0;
+    for(int i = grid.size() - 1; i >= 3; i--)
+    {
+        for(int j = 0; j < grid[i].size() - 4; j++)
+        {
+            int x = grid[i][j] * grid[i - 1][j + 1] * grid[i - 2][j + 2] * grid[i - 3][j + 3];
+            if(x > max)
+            {
+                max = x;
+            }
+        }
+    }
+    return max;
 }
 
 int check_br_to_tl(vector<vector<int>> &grid)
 {
+    int max  = 0;
+    for(int i = grid.size() - 1; i >= 3; i--)
+    {
+        for(int j = grid[i].size() - 1; j > 0; j--)
+        {
+            int x = grid[i][j] * grid[i - 1][j - 1] * grid[i - 2][j - 2] * grid[i - 3][j - 3];
+            if(x > max)
+            {
+                max = x;
+            }
+        }
+    }
+    return max;
 }
